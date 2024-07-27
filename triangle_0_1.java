df341905b037012a4553fb7c5071905acbce0b20
amish12822 <amishofficial23@gmail.com>
2024-07-27 21:19:14 +0530

    complete program 0-1 triangle

---
+++ triangle_0_1.java
@@ -0,0 +1,24 @@
+/**
+ * triangle_0_1
+ */
+public class triangle_0_1 {
+
+    public static void main(String[] args) {
+        
+        int a = 5;
+
+        for ( int i=1; i<=a; i++){
+
+            for ( int j=1; j<=i; j++){
+
+                if ( (i+j)%2 == 0) {
+                    System.out.print("1 ");
+                } else {
+                    System.out.print("0 ");
+                }
+            }
+
+            System.out.println();
+        }
+    }
+}
